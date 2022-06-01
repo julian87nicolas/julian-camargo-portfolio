d5194f902ebed3caefeae8abaec38d356a3db02d
julian87nicolas <julicmrgo@gmail.com>
2022-06-01 20:54:03 -0300

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
 
   return (
     <Routes>
-      <Route path ="*" element={<English/>} />
-      <Route path ="es*" element={<Spanish/>} />
+      <Route path ="/*" element={<English/>} />
+      <Route path ="/es*" element={<Spanish/>} />
     </Routes>
   );
 }
