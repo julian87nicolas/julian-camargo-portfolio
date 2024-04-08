c622d15458ad2afade960675d9bbd45a3ff843be
Julian <julian.camargo@sitrack.com>
2024-04-08 19:41:20 -0300

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,22 @@
 import "./styles/Contact.css"
 
-function Contact () {
+function Contact() {
     return (
         <>
-        <section id="contact">
-            <h2>Contact</h2>
-            <p>
-                Always interested in new growth opportunities.
-            </p>
-            <p>
-                <a href="mailto:julicmrgo@gmail.com">Email me</a>
-            </p>
+            <section id="contact">
+                <h2>Contact</h2>
+                <p>
+                    Always interested in new growth opportunities.
+                </p>
+                <p>
+                    <a href="mailto:julicmrgo@gmail.com">Email me</a> - <a href="https://www.linkedin.com/in/julian-camargo/" target="_blank" rel="noreferrer">
+                        LinkedIn
+                    </a>
+                </p>
 
-        </section>
-    
-        <div class="border-gradient"></div>
+            </section>
+
+            <div class="border-gradient"></div>
         </>
     )
 }
